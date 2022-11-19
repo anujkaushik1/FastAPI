5e3676326fe275b3cdeff4c45c3749c69b1b1186
Anuj Kaushik <anujkaushik1512@gmail.com>
2022-11-19 15:50:43 +0530

    Path Parameters

--- main.py
+++ main.py
@@ -2,6 +2,17 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+fakeDatabase = {
+    1 : {'task' : 'Clean Car'},
+    2 : {'task' : 'Write code'},
+    3 : {'task' : 'Start sleeping'},
+
+}
+
 @app.get("/")
 def getItems():
-    return ['Item1', 'Item2', 'Item3']
+    return fakeDatabase
+
+@app.get("/{id}")
+def getSingleItem(id : int):
+    return fakeDatabase[id]

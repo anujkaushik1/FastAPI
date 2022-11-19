36a12984a72bf7ffa603692c9be27dfa3dc3fd15
Anuj Kaushik <anujkaushik1512@gmail.com>
2022-11-19 16:46:33 +0530

    Post requst using pydantic

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import schemas
 
 app = FastAPI()
 
@@ -17,8 +18,15 @@ def getItems():
 def getSingleItem(id : int):       # id needs to be int only
     return fakeDatabase[id]
 
+# @app.post("/")
+# def addItems(task : str):        # we want task from body and needs to be string only
+#     newId = len(fakeDatabase.keys()) + 1
+#     fakeDatabase[newId] = {'task' : task}  # jo req se task aa rha hai usko fakedb mei daal rhe hai
+#     return fakeDatabase
+
+
 @app.post("/")
-def addItems(task : str):        # we want task from body and needs to be string only
+def addItems(item : schemas.Item):      # schema file ke andr Item Class  => item ek class ka object bnn gya  
     newId = len(fakeDatabase.keys()) + 1
-    fakeDatabase[newId] = {'task' : task}  # jo req se task aa rha hai usko fakedb mei daal rhe hai
+    fakeDatabase[newId] = {'task' : item.task} #Item.task
     return fakeDatabase
